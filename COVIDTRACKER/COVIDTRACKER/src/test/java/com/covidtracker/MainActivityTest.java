88d15530f24c3295f0c8fa5e14a3571f83306e88
raytr1602 <n01226282@humbermail.ca>
2020-12-05 21:45:59 -0500

    implementing tests

--- COVIDTRACKER/COVIDTRACKER/src/test/java/com/covidtracker/MainActivityTest.java
+++ COVIDTRACKER/COVIDTRACKER/src/test/java/com/covidtracker/MainActivityTest.java
@@ -1,13 +1,22 @@
 package com.covidtracker;
 
+import android.app.Activity;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MainActivityTest {
+    private static boolean onCreateCalled;
+    private static boolean onCreateDialogCalled;
+    private static boolean onClickCalled;
 
     @Test
     public void onCreate() {
+        if(!onCreateCalled) {
+            System.out.println("OnCreate is being called" +'\n'+ (10*10));
+            onCreateCalled = true;
+        }
     }
 
     @Test
@@ -16,5 +25,6 @@ public class MainActivityTest {
 
     @Test
     public void onBackPressed() {
+
     }
 }
