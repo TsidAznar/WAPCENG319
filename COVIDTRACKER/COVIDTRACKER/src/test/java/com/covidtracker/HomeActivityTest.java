28e66d4ce855bb63948853d1c16b2cc761ae3431
raytr1602 <n01226282@humbermail.ca>
2020-12-05 22:17:04 -0500

    implementing tests

---
+++ COVIDTRACKER/COVIDTRACKER/src/test/java/com/covidtracker/HomeActivityTest.java
@@ -0,0 +1,24 @@
+package com.covidtracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeActivityTest {
+
+    @Test
+    public void onStart() {
+    }
+
+    @Test
+    public void startNextActivity() {
+    }
+
+    @Test
+    public void onStop() {
+    }
+
+    @Test
+    public void onDestroy() {
+    }
+}
